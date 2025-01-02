41757677615d900aefb3452717e37956dc71c753
giulms <expertgiulliano@gmail.com>
2025-01-02 17:52:52 -0300

    Adição de arquivos atualizados

---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,63 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvalicoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.printf("Nome do filme: %s",nome);
+        System.out.printf("\nAno de lançamento: %d",anoDeLancamento);
+        System.out.printf("\nDuração em minutos: %d\n",duracaoEmMinutos);
+    }
+
+    public void avalia(double nota){
+        somaDasAvalicoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvalicoes/totalDeAvaliacoes;
+    }
+}
+
